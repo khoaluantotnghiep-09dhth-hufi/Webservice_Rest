f41980b11b5e94b3aa4e6f639abf9835c4db9e68
lqt2k <wintrung01@gmail.com>
2021-09-15 01:07:59 +0700

    Update CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,30 +8,71 @@ use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
-    //Lấy tất cả danh sách Customer
-    public function index()
+    //Lấy tất cả danh sách Customer theo bill
+    public function getCWB()
     {
-
         $result = DB::table('tbl_customer as c')
             ->join('tbl_bill as b', 'c.id', '=', 'b.id_customer')
             ->select('*')
             ->get();
         return response()->json($result);
     }
-    //Tạo một Customer
-    public function store($request)
+    //Lấy tất cả danh sách Customer
+    public function index()
     {
+        $result = DB::table("tbl_customer")
+        ->select('*')
+        ->get();
+        return response()->json($result);
+    }
+    //Tạo một Customer
+    public function store(Request $request)
+    {
+        DB::table('tbl_customer')
+        ->insert(
+            [
+                'name' => $request->name,
+                'address' => $request->address,
+                'phone' => $request->phone,
+                'image' => $request->image,
+                'password' => $request->password,
+                'email' => $request->email,
+            ]
+        );
+        return response()->json($request);
     }
     //Lấy một Customer theo $id
     public function show($id)
     {
+        $result = DB::table("tbl_customer")
+        ->where("id", "=", $id)
+        ->select('tbl_customer.id')
+        ->get();
+        return response()->json($result);
     }
     //Cập nhật một Customer theo $id
-    public function update($id)
+    public function update(Request $id)
     {
+        DB::table('tbl_customer')
+        ->where('id', $id->id)
+        ->update(
+            [
+                'name' => $id->name,
+                'address' => $id->address,
+                'phone' => $id->phone,
+                'image' => $id->image,
+                'password' => $id->password,
+                'email' => $id->email,
+            ]
+        );
+        return response()->json($id);
     }
     //Xóa một Customer theo $id
     public function destroy($id)
     {
+        DB::table('tbl_customer')
+        ->where('id', '=', $id)
+        ->delete();
+        return response()->json($id);
     }
 }
