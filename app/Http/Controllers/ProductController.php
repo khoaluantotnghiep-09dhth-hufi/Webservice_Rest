87762c982d1edd5c1858c192a451c6168b902b5d
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-14 09:31:15 +0700

    - Sửa ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,11 +12,11 @@ class ProductController extends Controller
     public function index()
     {
 
-        $result = DB::table('tbl_product as p')
-            ->join('tbl_category as c', 'c.id', '=', 'p.id_category')
-            ->join('tbl_promotion as pp', 'pp.id', '=', 'p.id_promotion')
-            ->join('tbl_product_info as ppf', 'ppf.id_product', '=', 'p.id')
-            ->select('p.id', 'p.name', 'p.price', 'p.description', 'p.like_product', 'p.id_category', 'c.name', 'p.image', 'p.id_promotion', 'pp.name', 'pp.date_start', 'pp.date_end', 'pp.desciption', 'ppf.id', 'ppf.id_size', 'ppf.id_color', 'ppf.quantity as quanityAll')->orderBy('p.id')
+        $result = DB::table('tbl_product as product')
+            ->join('tbl_category as c', 'c.id', '=', 'product.id_category')
+            ->join('tbl_promotion as pp', 'pp.id', '=', 'product.id_promotion')
+            ->join('tbl_product_info as ppf', 'ppf.id_product', '=', 'product.id')
+            ->select('product.id', 'product.name_product', 'product.price', 'product.description', 'product.like_product', 'product.id_category', 'c.name', 'product.image', 'product.id_promotion', 'pp.name', 'pp.date_start', 'pp.date_end', 'pp.desciption', 'ppf.id', 'ppf.id_size', 'ppf.id_color', 'ppf.quantity as quanityAll')->orderBy('product.id')
             ->get();
         return response()->json($result);
     }
