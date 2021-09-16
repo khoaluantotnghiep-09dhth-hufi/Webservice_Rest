8ee90916226a62580df2e4744d2587408cc1343b
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-16 14:10:55 +0700

    -Quan Ly Hoa Don

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -35,7 +35,8 @@ class BillController extends Controller
             ->where('id', $request->id)
             ->update(
                 [
-                    'status' => (int) $request->name,
+                    'status' => (int) $request->nameStatus,
+                    'date'=> $request->nameDate,
                 ]
 
             );
