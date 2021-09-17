7d6db356b91ed0c71b04319edfb78f72d9d11698
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-17 23:50:32 +0700

    - Thêm Thuộc Tính perSale Để tính Sale

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,6 +52,7 @@ class ProductController extends Controller
                 'tbl_category.name as nameCategory',
                 'tbl_product.image',
                 'tbl_promotion.name as namePromotion',
+                'tbl_promotion.desciption as percentSale',
                 'tbl_product_info.quantity',
                 'tbl_size.name as nameSize',
                 'tbl_color.name as nameColor'
