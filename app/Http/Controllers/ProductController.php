b5e2c2239b95cf3428382257c473c2ecf46ce61e
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-23 08:21:21 +0700

    - Thêm Mấy trường index2 Ảnh hướng đến cái product bên WEb Client. Nếu không được thì tách ra riêng 1 reducer để xử lý

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -71,7 +71,8 @@ class ProductController extends Controller
             ->join('tbl_category', 'tbl_category.id', '=', 'tbl_product.id_category')
             ->join('tbl_promotion', 'tbl_promotion.id', '=', 'tbl_product.id_promotion')
             ->join('tbl_product_info', 'tbl_product_info.id_product', '=', 'tbl_product.id')
-
+            ->join('tbl_color', 'tbl_color.id', '=', 'tbl_product_info.id_color')
+            ->join('tbl_size', 'tbl_size.id', '=', 'tbl_product_info.id_size')
             ->select(
                 'tbl_product.id',
                 'tbl_product.name',
@@ -82,7 +83,12 @@ class ProductController extends Controller
                 'tbl_category.name as nameCategory',
                 'tbl_product.image',
                 'tbl_promotion.name as namePromotion',
+                'tbl_promotion.desciption as percentSale',
+                'tbl_product_info.quantity as quantityAllProduct',
+                'tbl_size.name as nameSize',
+                'tbl_color.name as nameColor',
                 'tbl_product_info.id as id_product_info',
+
             )
             ->orderBy('tbl_product.id')
             ->get();
